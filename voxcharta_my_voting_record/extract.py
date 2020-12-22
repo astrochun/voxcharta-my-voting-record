38f4cb56e77cbf1f1cb9e6860b936325c042930a
Chun Ly <astro.chun@gmail.com>
2020-12-21 23:10:53 -0700

    Convert functional codes into Extract object

--- voxcharta_my_voting_record/extract.py
+++ voxcharta_my_voting_record/extract.py
@@ -4,131 +4,148 @@ import json
 import pandas as pd
 
 
-def import_data(filename):
+class Extract:
     """
-    Purpose: Import data
+    Purpose:
+      Extract metadata from VoxCharta My Voting Records HTML and
+      write JSON and CSV files:
 
     :param filename: str. Full path to MHTML
-
-    :return content: str
-    """
-
-    with open(filename, 'r') as f:
-        content = f.read()
-    f.close()
-
-    return content
-
-
-def soup_it(content):
-    """
-    Construct BeautifulSoup data structure
-
-    :param content: str (from import_data
-    :return page_content: BeautifulSoup4 data structure object
-    """
-    page_content = BeautifulSoup(content, "html.parser")
-
-    return page_content
-
-
-def get_records(page_content):
-    """
-    Retrieve records
-
-    :param page_content: BeautifulSoup4 data structure object
-    :return records_dict: dict containing records with keys in keys hierarchy
-    """
-
-    records = page_content.find_all('span', {'id': re.compile('votecount*')})
-    n_records = len(records)
-    print(f"Number of records: {n_records}")
-
-    # Note this is larger than [records] because of extra h3 heading at footer
-    h3 = page_content.find_all('h3')
-
-    postinfometa = page_content.find_all('span', {'class': 'postinfometa'})
-    postinfocats = page_content.find_all('span', {'class': 'postinfocats'})
-    abstract = page_content.find_all('div', {'class': 'post-content clearfix'})
-
-    # Get VoxCharta links, titles
-    records_dict = dict()
-
-    for ii in range(len(records)):
-        link = h3[ii].find('a')['href']
-        title = h3[ii].find('a').text
-        print(f"{ii} : {title}")
-
-        para = postinfometa[ii].find_all('p')
-        n_para = len(para)
-        authors = para[0].text
-        try:
-            affil = postinfometa[ii].find_next('p', {'class': 'metafoot'}).text
-        except AttributeError:
-            affil = ''  # No affiliation
-
-        # This handles discussion cases
-        if n_para > 3:
-            # This handles footnote for "Listed affiliation ..."
-            if 'Listed affiliation' in para[2].text:
-                arxiv = para[4]
-                comments = para[5].text
-            else:
-                arxiv = para[3]
-                comments = para[4].text
-            arxiv_id = arxiv.find('a').text
-
-            # get instruments
-            postinfocats[ii].text.split('\n')
-            categories_list = [val.replace(' ', '') for
-                               val in postinfocats[2].text.split('\n') if val]
-            categories = ';'.join(categories_list)
-            urls = arxiv.find_all('a')
-            abs_url = urls[0]['href']
-            pdf_url = urls[1]['href']
-            ps_url = urls[2]['href']
-            ads_url = urls[3]['href']
-            papers_url = urls[4]['href']
-            others_url = urls[5]['href']
-        else:
-            arxiv_id = f"no_id_{ii}"
-
-        records_dict[arxiv_id] = {
-            'link': link,
-            'title': title,
-            'authors': authors,
-            'affil': affil,
-            'abstract': abstract[ii].text.replace('\n', '')
-        }
-
-        # This handles discussion cases
-        if n_para > 3:
-            records_dict[arxiv_id].update({
-                'categories': categories,
-                'abs_url': abs_url,
-                'pdf_url': pdf_url,
-                'ps_url': ps_url,
-                'ads_url': ads_url,
-                'papers_url': papers_url,
-                'others_url': others_url,
-                'comments': comments
-            })
-
-    return records_dict
-
-
-def export_data(records_dict, json_outfile, csv_outfile):
-    """
-    Export JSON and csv content
-
-    :param records_dict: dict of records
     :param json_outfile: str for JSON output file
     :param csv_outfile: str for CSV output file
+
+    Attributes
+    ----------
+    content: str (from import_data)
+    page_content: BeautifulSoup4 data structure object (from soup_it)
+    records_dict: dict containing records with keys in keys hierarchy
+                  (from get_records)
+
+    Methods
+    -------
+    import_data(filename)
+      Import data
+
+    soup_it()
+      Construct BeautifulSoup data structure
+
+    get_records()
+      Retrieve records
+
+    export_data()
+      Write JSON and csv files
     """
 
-    print(f"Writing: {json_outfile}")
-    json.dumps(records_dict, json_outfile)
+    def __init__(self, filename, json_outfile, csv_outfile):
 
-    df = pd.DataFrame.from_dict(records_dict, orient='index')
-    print(f"Writing: {csv_outfile}")
-    df.to_csv(csv_outfile)
+        self.content = self.import_data(filename)
+        self.page_content = self.soup_it()
+        self.records_dict = self.get_records()
+        self.export_data(json_outfile, csv_outfile)
+
+    @staticmethod
+    def import_data(filename):
+        """Import data"""
+
+        with open(filename, 'r') as f:
+            content = f.read()
+        f.close()
+
+        return content
+
+    def soup_it(self):
+        """Construct BeautifulSoup data structure"""
+
+        page_content = BeautifulSoup(self.content, "html.parser")
+
+        return page_content
+
+    def get_records(self):
+        """Retrieve records"""
+
+        records = self.page_content.find_all('span', {'id': re.compile('votecount*')})
+        n_records = len(records)
+        print(f"Number of records: {n_records}")
+
+        # Note this is larger than [records] because of extra h3 heading at footer
+        h3 = self.page_content.find_all('h3')
+
+        postinfometa = self.page_content.find_all('span', {'class': 'postinfometa'})
+        postinfocats = self.page_content.find_all('span', {'class': 'postinfocats'})
+        abstract = self.page_content.find_all('div', {'class': 'post-content clearfix'})
+
+        # Get VoxCharta links, titles
+        records_dict = dict()
+
+        for ii in range(len(records)):
+            link = h3[ii].find('a')['href']
+            title = h3[ii].find('a').text
+            print(f"{ii} : {title}")
+
+            para = postinfometa[ii].find_all('p')
+            n_para = len(para)
+            authors = para[0].text
+            try:
+                affil = postinfometa[ii].find_next('p', {'class': 'metafoot'}).text
+            except AttributeError:
+                affil = ''  # No affiliation
+
+            # This handles discussion cases
+            if n_para > 3:
+                # This handles footnote for "Listed affiliation ..."
+                if 'Listed affiliation' in para[2].text:
+                    arxiv = para[4]
+                    comments = para[5].text
+                else:
+                    arxiv = para[3]
+                    comments = para[4].text
+                arxiv_id = arxiv.find('a').text
+
+                # get instruments
+                postinfocats[ii].text.split('\n')
+                categories_list = [val.replace(' ', '') for
+                                   val in postinfocats[2].text.split('\n') if val]
+                categories = ';'.join(categories_list)
+                urls = arxiv.find_all('a')
+                abs_url = urls[0]['href']
+                pdf_url = urls[1]['href']
+                ps_url = urls[2]['href']
+                ads_url = urls[3]['href']
+                papers_url = urls[4]['href']
+                others_url = urls[5]['href']
+            else:
+                arxiv_id = f"no_id_{ii}"
+
+            records_dict[arxiv_id] = {
+                'link': link,
+                'title': title,
+                'authors': authors,
+                'affil': affil,
+                'abstract': abstract[ii].text.replace('\n', '')
+            }
+
+            # This handles discussion cases
+            if n_para > 3:
+                records_dict[arxiv_id].update({
+                    'categories': categories,
+                    'abs_url': abs_url,
+                    'pdf_url': pdf_url,
+                    'ps_url': ps_url,
+                    'ads_url': ads_url,
+                    'papers_url': papers_url,
+                    'others_url': others_url,
+                    'comments': comments
+                })
+
+        return records_dict
+
+    def export_data(self, json_outfile, csv_outfile):
+        """Write JSON and csv files"""
+
+        print(f"Writing: {json_outfile}")
+        with open(json_outfile, 'w') as outfile:
+            json.dump(self.records_dict, outfile)
+
+        df = pd.DataFrame.from_dict(self.records_dict, orient='index')
+        print(f"Writing: {csv_outfile}")
+        df.to_csv(csv_outfile)
